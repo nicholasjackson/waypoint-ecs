c76ef7c6dccb9ac57e9a0fa96690f621cdbbfca3
Nicholas Jackson <jackson.nic@gmail.com>
2022-03-31 16:22:42 +0100

    Test update

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(rw, "Hello YouTube v2")
+		fmt.Fprint(rw, "Hello World")
 	})
 
 	http.ListenAndServe(":3000", nil)
