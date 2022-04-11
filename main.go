625f8aec7165fe591aad5dea7932bd3f35ca5578
Nicholas Jackson <jackson.nic@gmail.com>
2022-04-11 13:09:48 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(rw, "Hello World v6")
+		fmt.Fprint(rw, "Hello World v7")
 	})
 
 	http.ListenAndServe(":3000", nil)
